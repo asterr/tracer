9f1556f8dc09905e4d32f961c81eea2a604428be
asterr <asterr@pobox.com>
2018-02-08 13:42:37 +0000

    Update tracer.js
    converted to use localStorage index

--- tracer.js
+++ tracer.js
@@ -1,11 +1,12 @@
 javascript: (function() {
     var links = new Array();
-    links[0] = "https://raw.githubusercontent.com/asterr/numbers/master/number01.png?" + Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
-    links[1] = "https://raw.githubusercontent.com/asterr/numbers/master/number02.png?" + Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
-    links[2] = "https://raw.githubusercontent.com/asterr/numbers/master/number03.png?" + Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
-    links[3] = "https://raw.githubusercontent.com/asterr/numbers/master/number04.png?" + Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
-    links[4] = "https://raw.githubusercontent.com/asterr/numbers/master/number05.png?" + Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
-    links[5] = "https://raw.githubusercontent.com/asterr/numbers/master/number06.png?" + Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
-    var i = Math.floor(Math.random() * links.length);
+    links[0] = "https://raw.githubusercontent.com/asterr/numbers/master/number01.png";
+    links[1] = "https://raw.githubusercontent.com/asterr/numbers/master/number02.png";
+    links[2] = "https://raw.githubusercontent.com/asterr/numbers/master/number03.png";
+    links[3] = "https://raw.githubusercontent.com/asterr/numbers/master/number04.png";
+    links[4] = "https://raw.githubusercontent.com/asterr/numbers/master/number05.png";
+    links[5] = "https://raw.githubusercontent.com/asterr/numbers/master/number06.png";
+    var i = localStorage.getItem('tracerindex') || 0;
+    localStorage.setItem('tracerindex', i);
     window.open(links[i])
 })()
