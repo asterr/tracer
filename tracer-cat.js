a0c42412cbc15e64b4a497914aacd3e0c90ce257
asterr <asterr@pobox.com>
2018-02-19 20:10:22 +0000

    Create tracer-cat.js

---
+++ tracer-cat.js
@@ -0,0 +1,5 @@
+javascript: (function() {
+    var links = new Array();
+    links[0] = "https://media.istockphoto.com/photos/pleased-gray-striped-cat-squeezed-his-eyes-shut-closeup-picture-id628935028?" + Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
+    window.open(links[0])
+})()
